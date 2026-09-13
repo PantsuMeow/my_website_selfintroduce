1dd0d5d9aaf43f1f0edbc9c1248b34a1699d256d
喵喵喵喵喵~ <133024060+PantsuMeow@users.noreply.github.com>
2026-09-13 19:54:10 +0800

    更新 tags.js

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -40,6 +40,7 @@ const TAG_LIST = [
   { name: '母胎单身',    url: '', weight: 2 },
   { name: '裤袜控',   url: '', weight: 2 },
   { name: '纯爱战士',     url: '', weight: 4 },
+  { name: '古典音乐',     url: '', weight: 3 },
   { name: 'Ciallo~',  url: 'https://blog.pantsu0721.top/ciallo/ciallo.html', weight: 3, color: 0 }
 ];
 
